7370cefbf1b305445f7ca7577a7738d2f92e5a4d
Supreme Raut <supremeraut94@gmail.com>
2024-12-14 14:38:49 +0545

    feat: added the coordinates of ships in the gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -12,6 +12,52 @@ class gameBoard {
       }
     }
   }
+  shipCordinates() {
+    this.ships.forEach((ship) => {
+      let validPosition = false;
+
+      while (!validPosition) {
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+        let axis = ["horiz", "vert"];
+        let choosedAxis = axis[Math.floor(Math.random() * 2)];
+        let possibleCordinates = [];
+
+        for (let i = 0; i < ship.length; i++) {
+          if (choosedAxis === "horiz") {
+            if (y + ship.length <= 10) {
+              possibleCordinates.push([x, y + i]);
+            } else {
+              possibleCordinates.push([x, y - i]);
+            }
+          } else if (choosedAxis === "vert") {
+            if (x + ship.length <= 10) {
+              possibleCordinates.push([x + i, y]);
+            } else {
+              possibleCordinates.push([x - i, y]);
+            }
+          }
+        }
+        let checkOvelap = this.isOverlapping(possibleCordinates);
+        if (!checkOvelap) {
+          ship.cordinates = possibleCordinates;
+          validPosition = true;
+        }
+      }
+    });
+  }
+  isOverlapping(possible) {
+    for (const possibleCord of possible) {
+      for (const ship of this.ships) {
+        for (const cordinate of ship.cordinates) {
+          if (cordinate.toString() == possibleCord.toString()) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
 
 export { gameBoard };
